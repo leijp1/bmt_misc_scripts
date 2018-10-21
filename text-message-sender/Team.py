79a0070e25d4c4807da81e3031837144e87f2af4
Xiaoyan Kang <kang_xiaoyan@berkeley.edu>
2018-10-21 15:47:22 -0700

    modified Team.py

--- text-message-sender/Team.py
+++ text-message-sender/Team.py
@@ -1,23 +1,40 @@
-class Student:
-	def __init__(self, student_id, name, team):
-		self.id = student_id
-		self.name = name
-		self.team = team
+class Team:
+	def __init__(self, team_id, team_name, coach_name, students_names, phone_numbers):
+		self.team_id = team_id
+		self.team_name = team_name
+		self.coach_name = coach_name
+		self.students_names = students_names
+		self.phone_numbers = phone_numbers
 
-	def set_student_id(student_id):
-		self.id = student_id
+	def set_team_id(team_id):
+		self.team_id = team_id
 
-	def set_student_name(name):
-		self.name = name
+	def set_team_name(team_name):
+		self.team_name = team_name
 
-	def set_team(team):
-		self.team = team
+	def set_coach_name(coach_name):
+		self.coach_name = coach_name
 
-	def get_student_id():
-		return self.id
+	def set_students_names(students_names):
+		self.students_names = students_names
 
-	def get_student_name():
-		return self.name
+	def set_phone_numbers(phone_numbers):
+		self.phone_numbers = phone_numbers
 
-	def get_team():
-		return self.team
+	def get_team_id():
+		return self.team_id
+
+	def get_team_name():
+		return self.team_name
+
+	def get_coach_name():
+		return self.coach_name
+
+	def get_students_names():
+		return self.students_names
+
+	def get_phone_numbers():
+		return self.phone_numbers
+
+	def send_message(message):
+		message.replace("[team_name]", self.team_name)
